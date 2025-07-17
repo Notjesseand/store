f27709f0fb2fd18559432479cc2c51df0fa944f2
Notjessand <jessennorom@gmail.com>
2025-07-17 11:12:59 +0100

    x

--- src/app/utils/stripe.ts
+++
@@ -1,6 +0,0 @@
-// app/utils/stripe.ts
-import Stripe from "stripe";
-
-export const stripe = new Stripe(
-  "sk_test_51RlU08P0Xk5vXGCCvwmGVn5ovmktlvOLeU9ucRb5XvZklxtr1R4ron3RgzDU0RMo7jtPtSSdKLmq0NlzNQqdP85e0010woAcdT"
-);
